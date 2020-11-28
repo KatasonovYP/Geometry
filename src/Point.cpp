b7e69a04f740f58cc64e678955cbc0ef9a5ef062
KatasonovYP <75150376+KatasonovYP@users.noreply.github.com>
2020-11-28 16:14:37 +0300

    Delete Point.cpp

--- src/Point.cpp
+++
@@ -1,7 +0,0 @@
-#include "Point.hpp"
-
-double length (const Point& a, const Point& b) {
-	double sideX = a.x - b.x;
-	double sideY = a.y - b.y;
-	return std::sqrt(sideX * sideX + sideY * sideY);
-}
