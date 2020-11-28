5d6cdd38a2d29aed286f88e08136d88ca730e9fa
KatasonovYP <75150376+KatasonovYP@users.noreply.github.com>
2020-11-28 16:15:06 +0300

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-#include "Point.hpp"
-
-int main() {
-    Point a(0, 0), b(1, 1);
-    std::cout << "It's work! \nMaybe right, i hope...\n\n";
-    std::cout << length(a, b);
-    return 0;
-}
